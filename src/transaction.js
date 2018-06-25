05302d50efef37ff0ad0310ca57b7f763652624e
d-wook <mrdongwook@gmail.com>
2018-06-25 22:36:17 +0900

    defined transaction classes

--- src/transaction.js
+++ src/transaction.js
@@ -1,3 +1,5 @@
+const CryptoJS = require("crypto-js");
+
 class TxOut {
     constructor(address, amount) {
         this.address = address;
@@ -29,3 +31,16 @@ class UTxOut {
 }
 
 const uTxOuts = [];
+
+const getTxId = tx => {
+    const txInContent = tx.txIns
+    .map(txIn => txIn.uTxOutId + txIn.uTxOutIndex)
+    .reduce((a, b) => a + b, "");
+
+    const txOutContent = tx.txOuts
+    .map(txOut => txOut.address + txOut.amount)
+    .reduce((a, b) => a + b, "");
+
+    return Crypto.SHA256(txInContent, txOutContent).toStrng();
+}
+
