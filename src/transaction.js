e2c3600c02b849b3edf2b76a7d1611d4acc03291
d-wook <mrdongwook@gmail.com>
2018-06-26 21:03:50 +0900

    signed the transaction

--- src/transaction.js
+++ src/transaction.js
@@ -1,4 +1,8 @@
 const CryptoJS = require("crypto-js");
+const EC = require("elliptic");
+const utils = require("./utils");
+
+const ec = new EC('secp256k1');
 
 class TxOut {
     constructor(address, amount) {
@@ -22,9 +26,9 @@ class Transaction {
 }
 
 class UTxOut {
-    constructor(uTxOutId, uTxOutIndex, address, amount) {
-        this.uTxOutId = uTxOutId;
-        this.uTxOutIndex = uTxOutIndex;
+    constructor(txOutId, txOutIndex, address, amount) {
+        this.txOutId = txOutId;
+        this.txOutIndex = txOutIndex;
         this.address = address;
         this.amount = amount;
     }
@@ -44,3 +48,23 @@ const getTxId = tx => {
     return Crypto.SHA256(txInContent, txOutContent).toStrng();
 }
 
+const findUTxOut = (txOutId, txOutIndex, uTxOutsList) => {
+    return uTxOutsList.find(
+        uTxOut => uTxOut.txOutId === txOutId && uTxOut.txOutIndex === txOutIndex
+    );
+}
+
+const signTxIn = (tx, txInIndex, privateKey, uTxOut) => {
+    const txIn = tx.txIns[txInIndex];
+    const dataToSign = tx.id;
+    // To do: find tx
+    const referencedUTxOut = findUTxOut(txIn.txOutId, tx.txOutIndex, uTxOuts);
+    if(referencedUTxOut === null) {
+        return;
+    }
+
+    const key = ec.keyFromPrivate(privateKey, "hex");
+    const signature = utils.toHexString(key.sign(dataToSign).toDER());
+
+    return signature;
+}
